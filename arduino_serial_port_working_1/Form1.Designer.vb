38625c691561a8e11218c56a3a56ee6285eb9bb3
skipbox <skipbox500@gmail.com>
2016-11-06 07:35:05 +0900

    Add project files.

---
+++ arduino_serial_port_working_1/Form1.Designer.vb
@@ -0,0 +1,157 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnSend = New System.Windows.Forms.Button()
+        Me.txtSend = New System.Windows.Forms.TextBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.SerialText = New System.Windows.Forms.TextBox()
+        Me.text_auto_com = New System.Windows.Forms.TextBox()
+        Me.text_auto_select = New System.Windows.Forms.Button()
+        Me.textComPort = New System.Windows.Forms.TextBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.butFindPort = New System.Windows.Forms.Button()
+        Me.but_check_port = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnSend
+        '
+        Me.btnSend.Font = New System.Drawing.Font("Facebook Letter Faces", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSend.Location = New System.Drawing.Point(643, 139)
+        Me.btnSend.Name = "btnSend"
+        Me.btnSend.Size = New System.Drawing.Size(259, 56)
+        Me.btnSend.TabIndex = 0
+        Me.btnSend.Text = "Send"
+        Me.btnSend.UseVisualStyleBackColor = True
+        '
+        'txtSend
+        '
+        Me.txtSend.Location = New System.Drawing.Point(643, 207)
+        Me.txtSend.Name = "txtSend"
+        Me.txtSend.Size = New System.Drawing.Size(259, 22)
+        Me.txtSend.TabIndex = 1
+        Me.txtSend.Text = "1"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(524, 80)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(223, 24)
+        Me.ComboBox1.TabIndex = 2
+        '
+        'SerialText
+        '
+        Me.SerialText.Location = New System.Drawing.Point(498, 245)
+        Me.SerialText.Multiline = True
+        Me.SerialText.Name = "SerialText"
+        Me.SerialText.Size = New System.Drawing.Size(198, 145)
+        Me.SerialText.TabIndex = 3
+        '
+        'text_auto_com
+        '
+        Me.text_auto_com.Location = New System.Drawing.Point(165, 61)
+        Me.text_auto_com.Multiline = True
+        Me.text_auto_com.Name = "text_auto_com"
+        Me.text_auto_com.Size = New System.Drawing.Size(143, 132)
+        Me.text_auto_com.TabIndex = 16
+        '
+        'text_auto_select
+        '
+        Me.text_auto_select.Location = New System.Drawing.Point(12, 296)
+        Me.text_auto_select.Name = "text_auto_select"
+        Me.text_auto_select.Size = New System.Drawing.Size(147, 49)
+        Me.text_auto_select.TabIndex = 15
+        Me.text_auto_select.Text = "text_auto_select"
+        Me.text_auto_select.UseVisualStyleBackColor = True
+        '
+        'textComPort
+        '
+        Me.textComPort.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.textComPort.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.textComPort.Location = New System.Drawing.Point(12, 193)
+        Me.textComPort.Name = "textComPort"
+        Me.textComPort.Size = New System.Drawing.Size(145, 36)
+        Me.textComPort.TabIndex = 14
+        Me.textComPort.Text = "textComPort"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 16
+        Me.ListBox1.Location = New System.Drawing.Point(12, 61)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(147, 132)
+        Me.ListBox1.TabIndex = 13
+        '
+        'butFindPort
+        '
+        Me.butFindPort.Location = New System.Drawing.Point(12, 12)
+        Me.butFindPort.Name = "butFindPort"
+        Me.butFindPort.Size = New System.Drawing.Size(147, 49)
+        Me.butFindPort.TabIndex = 12
+        Me.butFindPort.Text = "butFindPort"
+        Me.butFindPort.UseVisualStyleBackColor = True
+        '
+        'but_check_port
+        '
+        Me.but_check_port.Location = New System.Drawing.Point(12, 230)
+        Me.but_check_port.Name = "but_check_port"
+        Me.but_check_port.Size = New System.Drawing.Size(147, 45)
+        Me.but_check_port.TabIndex = 17
+        Me.but_check_port.Text = "check_port"
+        Me.but_check_port.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(914, 424)
+        Me.Controls.Add(Me.but_check_port)
+        Me.Controls.Add(Me.text_auto_com)
+        Me.Controls.Add(Me.text_auto_select)
+        Me.Controls.Add(Me.textComPort)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.butFindPort)
+        Me.Controls.Add(Me.SerialText)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.txtSend)
+        Me.Controls.Add(Me.btnSend)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnSend As Button
+    Friend WithEvents txtSend As TextBox
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents SerialText As TextBox
+    Friend WithEvents text_auto_com As TextBox
+    Friend WithEvents text_auto_select As Button
+    Friend WithEvents textComPort As TextBox
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents butFindPort As Button
+    Friend WithEvents but_check_port As Button
+End Class
---
+++ arduino_serial_port_working_1/Form1.vb
@@ -0,0 +1,107 @@
+﻿Public Class Form1
+    Dim WithEvents SerialPort As New IO.Ports.SerialPort
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        butFindPort_Click(sender, e)
+
+        'ConnectSerial("hhh_000")
+    End Sub
+
+    Private Sub ConnectSerial(port_to_try As String)
+        Try
+            SerialPort.BaudRate = 9600
+            'SerialPort.PortName = "COM5" 'notice how the ports are named? they HAVE to have COM in front of the number
+            SerialPort.PortName = port_to_try 'notice how the ports are named? they HAVE to have COM in front of the number
+            SerialPort.Open()
+            SerialPort.Write("1") 'write a one for testing
+            text_auto_select.Text = "success"
+            'it will error if it is already open
+        Catch
+            SerialPort.Close()
+            text_auto_select.Text = "error"
+        End Try
+
+        'need 2 success frist that port is available
+        'then if NO error ... check if you got a 1 back from the arduino
+
+    End Sub
+
+    Delegate Sub myMethodDelegate(ByVal [text] As String)
+    Dim myD1 As New myMethodDelegate(AddressOf myShowStringMethod)
+
+    Sub myShowStringMethod(ByVal myString As String)
+        'display text to our textbox called SerialText
+        SerialText.AppendText(myString)
+    End Sub
+
+    Private Sub SerialPort_DataReceived(ByVal sender As Object, ByVal e As System.IO.Ports.SerialDataReceivedEventArgs) Handles SerialPort.DataReceived
+        Dim str As String = SerialPort.ReadExisting()
+        Invoke(myD1, str)
+    End Sub
+
+    Private Sub btnSend_Click_1(sender As Object, e As EventArgs) Handles btnSend.Click
+        If SerialPort.IsOpen Then
+            SerialPort.Write(txtSend.Text)
+        Else
+            ConnectSerial(textComPort.Text) 'key
+            SerialPort.Write(txtSend.Text)
+        End If
+    End Sub
+
+    '============================
+    Private Sub butFindPort_Click(sender As Object, e As EventArgs) Handles butFindPort.Click
+        ListBox1.Items.Add("COM_fake_1")
+        ListBox1.Items.Add("COM_fake_2")
+        Dim i As Integer
+        For i = 0 To My.Computer.Ports.SerialPortNames.Count - 1
+            ListBox1.Items.Add(My.Computer.Ports.SerialPortNames(i))
+            ListBox1.Items.Add("COM1")
+            ListBox1.Items.Add("COM2")
+        Next
+
+
+
+        'ListBox1.Items.Add("ASf")
+    End Sub
+
+    Dim list_index_counter As Integer = -1
+
+    Private Sub but_check_port_Click(sender As Object, e As EventArgs) Handles but_check_port.Click
+        '' text_auto_com.Text = textComPort.Text
+        'test_port(text_auto_com.Text)
+
+        If (list_index_counter < (ListBox1.Items.Count - 1)) Then
+            list_index_counter = list_index_counter + 1
+            ListBox1.SelectedIndex = list_index_counter
+
+            text_auto_com.Text = textComPort.Text
+            ConnectSerial(text_auto_com.Text)
+            'need 2 success frist that port is available
+            'then if NO error ... check if you got a 1 back from the arduino
+
+
+        Else
+            'ListBox1.SelectedIndex = list_index_counter
+            list_index_counter = -1
+        End If
+
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        textComPort.Text = ListBox1.Text
+        'SerialPortSetUp()
+        'Dim i As Integer
+        ' For i = 0 To ListBox1.Items.Count - 1
+        ' text_auto_com.Text = text_auto_com.Text + ListBox1.Items(i) + vbCrLf
+        ' Next
+
+        ' For my_list_index As Integer = 0 To ListBox1.Items.Count - 1
+        'Dim l_text As String = CStr(ListBox1.Items(my_list_index))
+        '  Next
+
+
+    End Sub
+
+
+
+End Class
